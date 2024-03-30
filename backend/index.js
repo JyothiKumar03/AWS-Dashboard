e3cdd65a9506294d6baf55d61bd23c0ac8a1b23f
Jyothi Kumar <djdummala@gmail.com>
2024-03-30 11:21:19 +0530

    fixed cors

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "*",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     })
